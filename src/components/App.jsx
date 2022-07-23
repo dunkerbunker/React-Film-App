a7565080ea137d735fba20ca76ab7a44b5705722
Yoosuf Sayyid <cena26657@gmail.com>
2022-07-23 21:09:31 +0500

    t

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,37 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
+import useStyles from './styles';
+
 import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 // eslint-disable-next-line react/function-component-definition
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <MovieInformation />
-        </Route>
-        <Route exact path="/actors/:id">
-          <Actors />
-        </Route>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
