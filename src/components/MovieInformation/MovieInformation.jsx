64a257981fa66196c6295bf09ec5094c46265848
Yoosuf Sayyid <cena26657@gmail.com>
2022-08-03 16:04:06 +0500

    actores added

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -79,6 +79,39 @@ const MovieInformation = () => {
             </Link>
           ))}
         </Grid>
+        <Typography variant="h5" gutterBottom style={{marginTop: '10px'}}>
+            Overview
+        </Typography>
+        <Typography style={{marginBottom: '2rem'}}>
+          {data?.overview}
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+            Top Cast
+        </Typography>
+        <Grid item container spacing={2}>
+            {data && data.credits?.cast?.map((cast, i) => (
+              cast.profile_path && (
+                <Grid 
+                item 
+                key={cast.id} 
+                sx={4} 
+                md={2} 
+                component={Link} 
+                to={`/actors/${cast.id}`} 
+                style={{textDecoration: 'none'}}
+              >
+                <img className={classes.castImage} 
+                  src={`https://image.tmdb.org/t/p/w500/${cast.profile_path}`} 
+                  alt={cast.name} 
+                />
+                <Typography color="textPrimary">{cast?.name}</Typography>
+                <Typography color="textSecondary">
+                  {cast?.character?.split('/')[0]}
+                </Typography>
+              </Grid>
+              )
+            )).slice(0, 6)}
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -43,5 +43,12 @@ export default makeStyles((theme) => ({
         [theme.breakpoints.down('sm')]: {
             padding: '0.5rem 1rem',
         },
-    }
+    },
+    castImage: {
+        width: '100%',
+        maxWidth: '7em',
+        height: '8em',
+        objectFit: 'cover',
+        borderRadius: '10px',
+    },
 }));
