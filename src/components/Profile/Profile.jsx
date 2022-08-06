711ec84ae4cd15116eaa942ddfa8f29266b1a01b
Yoosuf Sayyid <cena26657@gmail.com>
2022-08-06 12:28:02 +0500

    dark mdoe next

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,13 +4,21 @@ import { useSelector } from 'react-redux';
 import { ExitToApp } from '@mui/icons-material';
 
 import { userSelector } from '../../features/auth';
+import { useGetListQuery } from '../../services/TMDB';
+import { RatedCards } from '..';
 
 // eslint-disable-next-line react/function-component-definition
 const Profile = () => {
 
   const { user } = useSelector(userSelector);
 
-  const favouriteMovies = [];
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({ listName: 'favorite/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1});
+  const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1});
+
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlisted();
+  }, [])
 
   const logout = () => {
     localStorage.clear();
@@ -25,11 +33,13 @@ const Profile = () => {
           Logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      { !favouriteMovies.length ? (
+      { !favoriteMovies?.results?.length && !watchlistMovies?.results?.length 
+      ? (
         <Typography variant="h5">Add favourites or watchlist some movies to see them here!</Typography>
       ) : (
         <Box>
-          FAVOURITE MOVIES
+          <RatedCards title="My Favorite Movies" data={favoriteMovies} />
+          <RatedCards title="My Watchlist" data={watchlistMovies} />
         </Box>
       )}
     </Box>
